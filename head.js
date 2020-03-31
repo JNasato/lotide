6a3ec6a5f0347de3b864fae8b9e2f77a9c6a9f7e
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-03-31 02:22:25 +0000

    add tail functionality

--- head.js
+++ head.js
@@ -16,4 +16,4 @@ const head = function (array) {
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["Just This"]), "Just This");
-assertEqual(head([]), undefined);
+assertEqual(head([]), undefined);       
---
+++ tail.js
@@ -0,0 +1,30 @@
+//Assert Function
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🤗 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//TAIL FUNCTION
+const tail = function (array) {
+  let newArray = array.slice(0);
+  newArray.shift();
+  return newArray;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+const one = ["WOW WOW"];
+tail(one);
+assertEqual(one.length, 0);
+
+const empty = [];
+tail(empty);
+assertEqual(empty.length, 0);
+
+
+
