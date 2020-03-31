6105b9ea2ffcdf8851a031992cf31ee49534d2b8
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-03-31 23:54:06 +0000

    eqArrays file and updated assertion function

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🤗  Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤬  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i of array1) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
