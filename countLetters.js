3be2e893429902ab780ef9979fa70304c9350ced
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-04-01 20:59:21 +0000

    added count letters function to lotide

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+//ASSERT FUNCTION
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🤗  Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤬  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//COUNT LETTERS FUNCTION
+
+const countLetters = function (string) {
+  let lettersCounted = {};
+  const letters = string.split(" ").join("");
+  for (let letter of letters) {
+    if (lettersCounted[letter]) {
+      lettersCounted[letter] += 1;
+    } else {
+      lettersCounted[letter] = 1;
+    }
+  }
+  return lettersCounted;
+}
+
+console.log(countLetters("lighthouse in the house"))
