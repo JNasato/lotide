a084771b9848ad19077544a850efc49a96e243d7
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-04-06 19:15:22 +0000

    refactor eqArrays with module exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-//ASSERT FUNCTION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🤗  Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🤬  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 //EQ ARRAYS
 const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
@@ -21,7 +12,4 @@ const eqArrays = function (array1, array2) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
